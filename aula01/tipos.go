93fdbb8b895e2465cb2a4ff96d1f906c81906297
Davyusow <dayvsonf27@gmail.com>
2025-12-05 00:13:18 -0300

    Tipos em Go

--- aula01/tipos.go
+++ aula01/tipos.go
@@ -6,8 +6,15 @@ var num int8 //ao declarar uma variável desta maneira, ela só poderar ter valo
 // dentro de um code block
 // Caso o tipo não tenha sido declarado, será definido dinâmicamente
 
+var zero int  //ao não ser inicializada, pode sofrer diversas mudanças
+//no caso do int é o 0 mesmo
+// Para functions, pointers, e outras coisas que usam o endereço, o valor zero é nil: NULO
+
 func main() {
-    num = 16
-    println(valor)
+    num = 16 //inicializa a variável e atribui
+    println(num)
+    num = 21 //atribui
+    println(num)
+    println(zero)
 
 }
