63b214216a15dfcf0d05c787087bcd924c2df013
Davyusow <dayvsonf27@gmail.com>
2025-12-14 22:55:16 -0300

    formatações

--- aula02/constantes.go
+++ aula02/constantes.go
@@ -1,20 +1,21 @@
 package main
 
 const x1 = 10
+
 var y = 10
 
-const (//outra notação de constantes
-    A rune = 'a'
-    B      = 'b'
-    C      = 'c'
+const ( //outra notação de constantes
+	A rune = 'a'
+	B      = 'b'
+	C      = 'c'
 )
 
-func main () {
-    a := 10
-    b := 10.0
+func main() {
+	a := 10
+	b := 10.0
 
-    println(x1)
-    println(y)
-    println(a)
-    println(b)
+	println(x1)
+	println(y)
+	println(a)
+	println(b)
 }
